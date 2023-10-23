f00c4c1fb8cf9d14db62556698e8ff3f10ad9914
Daniel Gaull <gaulldj@rose-hulman.edu>
2023-10-23 15:57:18 -0400

    Added express serving up public folder

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-
+import express from 'express';
 
 // Need to create static site
 // Need to write basic API for the site
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.\nURL: https://localhost:${PORT} \nPress Ctrl+C to terminate`);
+});
